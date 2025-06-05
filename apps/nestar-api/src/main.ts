67d9a1132d12bef1460d7ec9563e67d26ba511ac
Azi-png <azizagoibnazarova24@gmail.com>
2025-06-05 10:32:17 +0900

    fix: connect MongoDb database

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT_API ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
+console.log(' NODE_ENV =', process.env.NODE_ENV);
+console.log(' MONGO_PROD =', process.env.MONGO_PROD);
