09cd240fc40b653c431443ebc303d4c357c1925e
Azi-png <azizagoibnazarova24@gmail.com>
2025-06-05 11:07:42 +0900

    feat: task ZK is done

---
+++ train.ts
@@ -0,0 +1,21 @@
+/*TASK ZK:
+
+Shunday function yozing, bu function har bir soniyada bir marotaba
+console'ga 1'dan 5'gacha bo'lgan raqamlarni chop etsin va
+5 soniyadan so'ng function o'z ishini to'xtatsin
+  
+MASALAN: printNumbers();*/
+function printNumbers() {
+	let count = 1;
+
+	const interval = setInterval(() => {
+		console.log(count);
+		count++;
+
+		if (count > 5) {
+			clearInterval(interval);
+		}
+	}, 1000);
+}
+
+printNumbers();
